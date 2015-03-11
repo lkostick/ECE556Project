109e70d0f2c328d0652d9e137fea633c0db54daa
lkostick <lkostick@wisc.edu>
2015-03-11 18:52:02 -0500

    release implementation

--- projectPart1/ece556-src/ece556.cpp
+++ projectPart1/ece556-src/ece556.cpp
@@ -78,23 +78,19 @@ int writeOutput(const char *outRouteFile, routingInst *rst){
 
 int release(routingInst *rst){
   /*********** TO BE FILLED BY YOU **********/
-	int i = j = k = 0;
-	int sizeNets = sizePins = sizeSegments = 0;
+	int i,j;
+	int sizeNets,sizeSegments;
 	if(rst == NULL) return 0;
 	sizeNets = rst->numNets;
 	for(i = 0; i < sizeNets; i++){ // Iterate through all the nets in the routing instance
-		sizePins = rst->nets[i].numPins;
-		for(j = 0; j < sizePins; j++){ // Iterate through the pins for each net struct
-			free(rst->nets[i].pins[j]);
-		}
+		free(rst->nets[i].pins);
 		sizeSegments = rst->nets[i].nroute.numSegs;
-		for(k = 0; k < sizeSegments; k++){ // Iterate through the segements in the route 
-			free(rst->nets[i].nroute.segments[k].edges);
-			free(rst->nets[i].nroute.segments[k]);
+		for(j = 0; j < sizeSegments; j++){ // Iterate through the segements in the route 
+			free(rst->nets[i].nroute.segments[j].edges);
 		}
-		free(rst->nets[i].nroute);
-		free(rst->nets[i]);
+		free(rst->nets[i].nroute.segments);
 	}
+	free(rst->nets);
 	free(rst->edgeCaps);
 	free(rst->edgeUtils);
 	free(rst);
